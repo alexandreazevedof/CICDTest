c70d1d0e906332fd7d1464666b47add38a4811b6
Alexandre Azevedo Filho <alexandreazevedof@gmail.com>
2022-11-18 16:03:44 -0600

    Update and rename after-install.bat to after-install.ps1

--- CICDTest/codedeploy/after-install.bat
+++
@@ -1,3 +0,0 @@
-powershell.exe -Command Add-PSSnapin WDeploySnapin3.0
-
-powershell.exe -Command Restore-WDPackage "C:\inetpub\wwwroot\CICDTest.zip" -Parameters @{"IIS Web Application Name"="cicdtest"}
